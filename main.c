fbe76b09f6d785bfee4b8ffba4df8d0612580730
chaewon0902 <144991565+chaewon0902@users.noreply.github.com>
2023-10-01 13:27:36 +0900

    5

--- main.c
+++ main.c
@@ -5,24 +5,21 @@
 
 int main(int argc, char *argv[]) 
 {
-	int a;
-	printf("input a year :");
-	scanf("%i", &a);
+	unsigned int x;
+	int b;
 	
-	if ( (a%4==0 && a%100!=0) || (a%400==0) )
+	printf("input a number :");
+	scanf("%ui", &x);
+	
+	for (b=0; x!=0; x>>=1) // -> x = x >> 1
 	{
-		printf("%i is leap year. \n", a);
+		if ( x&1 )
+		{
+			b++;
+		}
 	}
-	else
-	{
-		printf("%i is not leap year. \n", a);
-	}
-	//1. 4로 나누어 떨어져야 함
 	
-	//2. 100으로 나누어 떨어지지 않아야 함
-	
-	//3. 위를 만족 안해도 400으로 나누어 떨어짐 
-	
+	printf("The result is : %i\n", b);
 	
 	return 0;
 }
